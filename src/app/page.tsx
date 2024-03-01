8619c9e196b6c40a1334ca88e262e7ae183c2cee
Edouard ROUSSEL <pro.edouard.roussel@gmail.com>
2024-03-01 15:18:45 +0100

    mobile version - first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="bg-[#EEEEEE]">
       <section className="lg:container lg:mx-auto lg:px-4 pt-20 h-screen ">
         <div className="grid grid-cols-7 grid-rows-7 w-full h-full relative">
-          <div className="col-span-7 col-start-1 row-start-1 row-span-3 md:row-span-4 relative">
+          <div className="col-span-7 col-start-1 row-start-1 row-span-4 md:row-span-4 relative">
             <div
               className="bg-[length:270%] md:bg-[length:190%] lg:bg-[length:150%] xl:bg-[length:120%] 2xl:bg-[length:100%] w-full h-full"
               style={{
@@ -16,8 +16,8 @@ export default function Home() {
               }}
             ></div>
             <div className="bg-black absolute inset-0 opacity-20"></div>
-            <div className="absolute inset-0 flex flex-col justify-between md:p-10 px-4 py-6 [text-shadow:_0_1px_0_rgb(0_0_0_/_12%)]">
-              <h6 className="text-white font-bold md:text-5xl text-3xl">
+            <div className="absolute inset-0 flex flex-col justify-between p-10 [text-shadow:_0_1px_0_rgb(0_0_0_/_12%)]">
+              <h6 className="text-white font-bold md:text-5xl text-2xl">
                 Pour réduire ma consommation d&apos;énergie, pas besoin de
                 diviser ma maison !
               </h6>
@@ -45,7 +45,7 @@ export default function Home() {
               </ul>
               <Button
                 content="Je veux être appelé !"
-                className="w-8/12 self-center md:self-start md:w-fit"
+                className="w-8/12 self-center md:self-start md:w-fit text-sm md:text-base"
               />
             </div>
           </div>
