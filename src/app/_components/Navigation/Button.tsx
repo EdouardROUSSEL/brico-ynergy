7b1dc053474a02746a52f5380f0e4716bdc65a70
Edouard ROUSSEL <pro.edouard.roussel@gmail.com>
2024-03-01 14:54:27 +0100

    first commit

--- src/app/_components/Navigation/Button.tsx
+++ src/app/_components/Navigation/Button.tsx
@@ -8,7 +8,7 @@ const Button: React.FC<{
   className?: string;
   href?: string;
 }> = ({ content, className, href }) => {
-  const buttonClassName = `bg-black text-white rounded-full px-5 py-2 h-12 flex justify-center items-center ${className}`;
+  const buttonClassName = `bg-black text-white rounded-full md:px-5 md:py-2 md:py-3 px-4 py-2 text-sm md:text-base flex justify-center items-center w-fit ${className}`;
   return (
     <button className={buttonClassName}>
       {typeof content === "string" ? (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,53 @@
 import Image from "next/image";
+import Button from "./_components/Navigation/Button";
 
 export default function Home() {
   return (
     <main className="bg-[#EEEEEE]">
       <section className="lg:container lg:mx-auto lg:px-4 pt-20 h-screen ">
         <div className="grid grid-cols-7 grid-rows-7 w-full h-full relative">
-          <div className="col-span-7 col-start-1 row-start-1 row-span-3 relative">
-            <h6 className="row-start-1 col-span-3 text-white font-bold text-5xl absolute z-10 p-10 ">
-              Pour réduire ma consommation d&apos;énergie, <br />
-              pas besoin de diviser ma maison !
-            </h6>
-            <ul className="absolute z-10">
-              <li>Offre en exclu sur le web !</li>
-              <li>Rendez-vous le 4 mars pour en savoir plus !</li>
-            </ul>
+          <div className="col-span-7 col-start-1 row-start-1 row-span-4 relative">
             <div
-              className="md:hidden block bg-no-repeat w-full h-full "
+              className="bg-[length:270%] md:bg-[length:190%] lg:bg-[length:150%] xl:bg-[length:120%] 2xl:bg-[length:100%] w-full h-full"
               style={{
                 backgroundImage: "url(./../images/hh.jpg)",
-                backgroundPosition: "50% 66%",
-                backgroundSize: "270%",
+                backgroundPosition: "55% 66%",
                 transform: "scaleX(-1)",
               }}
             ></div>
-
-            <div
-              className="bg-no-repeat w-full h-full hidden md:block lg:hidden"
-              style={{
-                backgroundImage: "url(./../images/hh.jpg)",
-                backgroundPosition: "50% 66%",
-                backgroundSize: "190%",
-                transform: "scaleX(-1)",
-              }}
-            ></div>
-            <div
-              className="lg:block hidden xl:hidden bg-no-repeat w-full h-full "
-              style={{
-                backgroundImage: "url(./../images/hh.jpg)",
-                backgroundPosition: "50% 66%",
-                backgroundSize: "150%",
-                transform: "scaleX(-1)",
-              }}
-            ></div>
-            <div
-              className="xl:block 2xl:hidden hidden bg-no-repeat w-full h-full "
-              style={{
-                backgroundImage: "url(./../images/hh.jpg)",
-                backgroundPosition: "50% 66%",
-                backgroundSize: "120%",
-                transform: "scaleX(-1)",
-              }}
-            ></div>
-            <div
-              className="2xl:block hidden bg-no-repeat w-full h-full "
-              style={{
-                backgroundImage: "url(./../images/hh.jpg)",
-                backgroundPosition: "0% 66%",
-                backgroundSize: "100%",
-                transform: "scaleX(-1)",
-              }}
-            ></div>
-            <div className="absolute inset-0 bg-black opacity-20"></div>
+            <div className="bg-black absolute inset-0 opacity-20"></div>
+            <div className="absolute inset-0 flex flex-col justify-between md:p-10 p-4 [text-shadow:_0_1px_0_rgb(0_0_0_/_12%)]">
+              <h6 className="text-white font-bold md:text-5xl text-2xl">
+                Pour réduire ma consommation d'énergie, pas besoin de diviser ma
+                maison !
+              </h6>
+              <ul className="text-white text-sm md:text-xl font-bold p-2 md:p-8 space-y-2">
+                <li className="flex items-center gap-2 md:gap-4">
+                  <Image
+                    width="20"
+                    height="20"
+                    src="/images/verifier.png"
+                    alt=""
+                    className="w-4 md:w-6"
+                  />
+                  Offre en exclu sur le web !
+                </li>
+                <li className="flex items-center gap-2 md:gap-4">
+                  <Image
+                    width="20"
+                    height="20"
+                    src="/images/verifier.png"
+                    alt=""
+                    className="w-4 md:w-6"
+                  />
+                  Rendez-vous le 4 mars pour en savoir plus !
+                </li>
+              </ul>
+              <Button
+                content="Je veux être appelé !"
+                className="w-8/12 self-center md:self-start md:w-fit"
+              />
+            </div>
           </div>
         </div>
       </section>
