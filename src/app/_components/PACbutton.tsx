47251691c358c7fc3ccc0801040eaf0e4dd657c8
Edouard ROUSSEL <pro.edouard.roussel@gmail.com>
2024-03-13 11:09:53 +0100

    finale version

--- src/app/_components/PACbutton.tsx
+++ src/app/_components/PACbutton.tsx
@@ -2,17 +2,17 @@ import Button from "./Navigation/Button";
 
 export default function PACbutton() {
   return (
-    <div className="fixed bottom-4 md:bottom-8 md:right-8 right-4 z-30">
+    <div className="fixed bottom-4 right-4 z-30 md:bottom-8 md:right-8">
       <div className="relative flex">
-        <button className="active:opacity-90 shadow-lg hover:bg-[#DF371E] bg-black text-white rounded-full md:px-5  md:py-3 px-4 py-2 text-base flex justify-center items-center w-fit">
+        <button className="flex w-fit items-center justify-center rounded-full bg-black px-4  py-2 text-base text-white shadow-lg hover:bg-[#DF371E] active:opacity-90 md:px-5 md:py-3">
           Simuler mon projet
         </button>
-        <div className="absolute right-3 justify-center items-center flex">
+        <div className="absolute right-3 flex items-center justify-center">
           <span
-            className="animate-ping absolute  inline-flex h-5 w-5 rounded-full bg-[#DF371E] opacity-75"
+            className="absolute inline-flex  h-5 w-5 animate-ping rounded-full bg-[#F7981C] opacity-75"
             style={{ animationDuration: "2s" }}
           ></span>
-          <span className="absolute inline-flex rounded-full h-3 w-3 bg-[#DF371E]"></span>
+          <span className="absolute inline-flex h-3 w-3 rounded-full bg-[#F7981C]"></span>
         </div>
       </div>
     </div>
